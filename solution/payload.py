1d4365356aa83918c164a115801611eef15a531e
Michalis Papadopoullos <mxpapad@pm.me>
2020-11-28 14:33:02 +0200

    2020-11-28

--- solution/payload.py
+++ solution/payload.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
 
 USE_UNICODE = True          # use UNICODE or OCTAL
-    
-is_next_safe = False        # if next character is in ['A' .. 'F'] unicode representation should be padded        
+# if next character is in ['A' .. 'F'] unicode representation should be padded
+
 while True:
+    is_next_safe = True       
     cmd = input("> ")
     lst = ["$'"]
     cmd_len = len(cmd)
     next_char = ''
-    for i in range(cmd_len):
-        if i < cmd_len - 1:
-            next_char = cmd[i + 1]
-        is_next_safe = not( ord('A') <= ord(next_char) <= ord('F') )
-        cc = ord(cmd[i])
+    for ix, c in enumerate(cmd):
+        if ix < cmd_len - 1:
+            next_char = cmd[ix + 1]
+            is_next_safe = not( ord('A') <= ord(next_char) <= ord('F') )
+        cc = ord(c)
         if ord('a') <= cc <= ord('z'):
-            lst.append( (("\\U{:x}" if is_next_safe else "\\U{:08x}") if USE_UNICODE else "\\{:o}").format(cc) )
+            lst.append( (("\\U{:x}" if is_next_safe else "\\U{:08x}") \
+                if USE_UNICODE else "\\{:o}").format(cc) )
         else:
-            lst.append( cmd[i] )
+            lst.append( c )
     lst.append( "'" )
     print( ''.join(lst) )
